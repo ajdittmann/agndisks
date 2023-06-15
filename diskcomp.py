73cc09841c7948c15d498bb8f8f7a72eba907d15
ajdittmann <dittmann@astro.umd.edu>
2023-06-15 11:44:46 -0400

    added support for tabulated opacities

--- diskcomp.py
+++ diskcomp.py
@@ -5,9 +5,10 @@ matplotlib.rc('text', usetex=True)
 import matplotlib.pyplot as plt
 from numba import njit  #improves things if Nr > 10^5 or so
 from scipy import optimize as sciop
+from scipy import interpolate as scint
 
 # resolution
-rmax = 10**4.0	#maximum radius in Rg
+rmax = 10**6.0	#maximum radius in Rg
 rmin = 10**1.0	#minimum radius in Rg
 Nr = 10000
 
@@ -18,6 +19,10 @@ alpha = 0.1	# effective viscosity parameter
 X = 0.72	# hydrogen mass fraction
 Z = 0.02	# 'metals' mass fraction
 eps = 0.1	# efficiency of rest mass -> energy for eddington accretion rate
+
+#opacity table - set to None for Kramers opacity
+#opacTable = None
+opacTable = "combined-opacity-main/opacitysolar09dustq3p5amax0p1new.txt"
 
 
 #physical constants
@@ -43,11 +48,43 @@ def Omega(r, M):
   return np.sqrt(G*M/r**3)
 
 @njit
-def opacity(rho, T):
+def opacKramer(rho, T):
   kk = 4.0*10**25*(X+1)*(Z + 0.001)*rho*T**-3.5
   return kes + kk
 
-@njit
+if opacTable is not None:
+  with open(opacTable,'r') as fp:
+    line=fp.readline()
+    numbers_str = line.split()
+    nrho=int(numbers_str[1])
+    nt=int(numbers_str[3])
+    rhoread=np.zeros(nrho)
+    treadnew=np.zeros(nt)
+    rosscombineread=np.zeros([nt,nrho])
+    planckcombineread=np.zeros([nt,nrho])
+    for irho in range(nrho):
+        for it in range(nt):
+            line=fp.readline()
+            numbers_str = line.split()
+            rhoread[irho]=float(numbers_str[0])
+            treadnew[it]=float(numbers_str[1])
+            rosscombineread[it,irho]=float(numbers_str[2])
+  #opacityinter = scint.interp2d(np.log10(rhoread), np.log10(treadnew), np.log10(rosscombineread), kind='linear')
+  opacityinter = scint.RectBivariateSpline(np.log10(rhoread), np.log10(treadnew), np.log10(rosscombineread.T), kx=1, ky=1 ) #kind='linear')
+  maxT = np.max(treadnew)
+
+  def opacTabulated(rho, T):
+    if T > maxT: return kes
+    #else: return 10.0**opacityinter(np.log10(rho), np.log10(T))
+    else: return 10.0**opacityinter.ev(np.log10(rho), np.log10(T))
+
+def opacity(rho, T):
+  if opacTable is not None:
+    opac = opacTabulated(rho, T)
+  else:
+    opac = opacKramer(rho, T)
+  return opac
+
 def rhosolve(rho_g, tau, T, B):
   kappa = opacity(rho_g, T)
   return tau - kappa*B**(1./3.)*rho_g**(2./3.)
@@ -59,11 +96,12 @@ def psolve(T, rho, P):
 
 def tsolve(Tg, A, B, omega, rho_g):
   tau = Tg**4/A
-  rho = sciop.fsolve(rhosolve, x0=rho_g, args = (tau, Tg, B) )
+  res = sciop.fsolve(rhosolve, x0=rho_g, args = (tau, Tg, B), full_output=1)
+  rho = res[0]
   H = (B/rho)**(1./3.)
   P = rho*H**2*omega**2
-  T = sciop.fsolve(psolve, x0 = Tg, args=(rho,P))
-  return T - Tg
+  res = sciop.fsolve(psolve, x0 = Tg, args=(rho,P), full_output=1)
+  return res[0] - Tg
 
 def getvals(r, T_g = None, rho_g=None):
   omega = Omega(r, MBH)
@@ -74,35 +112,54 @@ def getvals(r, T_g = None, rho_g=None):
     T_g = omega*(3/a)**0.5*(B/(A*kes))**0.25
   if rho_g is None:
     rho_g = omega**6*B*(3/(a*A*kes))**3
-  res = sciop.fsolve(tsolve, x0=T_g, args=(A, B, omega, rho_g))
-  T = res[0]
+  resT = sciop.fsolve(tsolve, x0=T_g, args=(A, B, omega, rho_g), full_output=1)
+  T = resT[0]
+  tierr = resT[2]
   tau = T**4/A
-  resrho = sciop.fsolve(rhosolve, x0=rho_g, args = (tau, T_g, B) )
+  resrho = sciop.fsolve(rhosolve, x0=rho_g, args = (tau, T_g, B), full_output=1 )
   rho = resrho[0]
+  rhoierr = resrho[2]
   H = (B/rho)**(1./3.)
   P = rho*H**2*omega**2
-  return T, rho, H, P, tau
+  return T, rho, H, P, tau, tierr, rhoierr
 
-rs = np.linspace(rmin, rmax, Nr)*Rg
+rs = 10.0**np.linspace(np.log10(rmin), np.log10(rmax), Nr)*Rg
 ts = np.empty(Nr)
 rhos = np.empty(Nr)
 hs = np.empty(Nr)
 ps = np.empty(Nr)
 taus = np.empty(Nr)
+badTs = np.empty(Nr)
+badRhos = np.empty(Nr)
 for i in range(Nr):
-  if i==0:  Ti, rhoi, Hi, Pi, taui = getvals(rs[i])
-  else:  Ti, rhoi, Hi, Pi, taui = getvals(rs[i], ts[i-1], rhos[i-1])
+  if i==0:  Ti, rhoi, Hi, Pi, taui, terr, rerr = getvals(rs[i])
+  else:  Ti, rhoi, Hi, Pi, taui, terr, rerr = getvals(rs[i], ts[i-1], rhos[i-1])
   ts[i] = Ti
   rhos[i] = rhoi
   hs[i] = Hi
   taus[i] = taui
   ps[i] = Pi
+  badTs[i] = terr
+  badRhos[i] = rerr
+
+#only use radii where it actually found a valid solution
+good = (badTs==1)&(badRhos==1)
+print(np.sum(good))
+
+rs = rs[good]
+ts = ts[good]
+rhos = rhos[good]
+hs = hs[good]
+taus = taus[good]
+ps = ps[good]
+Nr = np.sum(good)
 
 omegas = Omega(rs, MBH)
 cs = hs*omegas
-kappas = opacity(rhos, ts)
+kappas = taus/(rhos*hs)
 sigmas = 2*hs*rhos
 Qs = cs*omegas/(np.pi*sigmas*G)
+
 
 def fwd(rg):
   return rg*Rg/(3.086*10**18)
@@ -119,7 +176,8 @@ ax[3,0].loglog(rs/Rg, cs)
 ax00pc = ax[0,0].secondary_xaxis('top', functions=(fwd, bwd))
 
 ax[0,1].loglog(rs/Rg, kappas)
-ax[1,1].loglog(rs/Rg, rhos*hs*kappas)
+ax[1,1].loglog(rs/Rg, taus)
+ax[1,1].loglog(rs/Rg, np.ones(Nr), color='black', ls = "--")
 ax[2,1].loglog(rs/Rg, hs/rs)
 ax[3,1].loglog(rs/Rg, Qs)
 ax[3,1].loglog(rs/Rg, np.ones(Nr), color='black', ls = "--")
@@ -144,7 +202,4 @@ ax[1,1].set_ylabel(r'$\tau$')
 ax[2,1].set_ylabel(r'$H/r$',labelpad=-2)
 ax[3,1].set_ylabel(r'$Q$')
 
-
-
 plt.savefig('diskmodel.pdf')
-
